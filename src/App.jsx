b1502d51fb8a11797b7cfd2ee1dd44782ec2e495
ToshikiShitamichi <t.shitamichi0123@gmail.com>
2026-01-18 13:21:04 +0900

    0118講義

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,42 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import News from './components/NEWS'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // useState宣言
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+
+  // イベント処理
+  const handleNameChange = (e) => {
+    setName(e.target.value)
+  }
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value)
+  }
+
+  
+  useEffect(() => {
+    console.log("起動した！")
+  },[])
+  
+  console.log("順番の確認")
 
   return (
     <>
+      {/*  */}
+      <News/>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <p>名前が入ります</p>
+        <input type="text" placeholder='名前を入力してください' value={name} onChange={handleNameChange} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <p>メールアドレスが入ります</p>
+        <input type="text" placeholder='メールアドレスを入力してください' value={email} onChange={handleEmailChange} />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/*  */}
     </>
   )
 }
