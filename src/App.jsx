ef2900e16398470de516cd90ae89c65ced82efd5
ToshikiShitamichi <t.shitamichi0123@gmail.com>
2026-01-21 13:31:31 +0900

    0118課題

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,22 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import News from './components/News'
+import Chart from "./pages/chart";
+import Home from "./pages/home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NotFound from "./pages/notfound";
 
 function App() {
-  // useState宣言
-  const [name, setName] = useState("")
-  const [email, setEmail] = useState("")
-
-  // イベント処理
-  const handleNameChange = (e) => {
-    setName(e.target.value)
-  }
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value)
-  }
-
-
-  useEffect(() => {
-    console.log("起動した！")
-  }, [])
-
-  console.log("順番の確認")
-
   return (
     <>
       {/*  */}
-      <News />
-      <div>
-        <p>名前が入ります</p>
-        <input type="text" placeholder='名前を入力してください' value={name} onChange={handleNameChange} />
-      </div>
-      <div>
-        <p>メールアドレスが入ります</p>
-        <input type="text" placeholder='メールアドレスを入力してください' value={email} onChange={handleEmailChange} />
-      </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/chart" element={<Chart />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
       {/*  */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
